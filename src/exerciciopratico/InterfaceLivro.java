ef0abe8bf2232132db0cf973c82fdcfd13e921cb
Victor Braz <vic_braz@outlook.com>
2021-11-16 15:06:58 -0300

    Estudo Poo - Pessoa lê livro (Encapsulamento e Agregação)

---
+++ src/exerciciopratico/InterfaceLivro.java
@@ -0,0 +1,11 @@
+package exerciciopratico;
+
+// Interface Livro
+public interface InterfaceLivro 
+{
+    public abstract void abrir();
+    public abstract void fechar();
+    public abstract void folhear(int a);
+    public abstract void avancarPag();
+    public abstract void voltarPag();
+}
